57211495a1718775d0d748b31694f2c6f0b9774f
Pranav <pranavpawar745@gmail.com>
2024-11-09 00:08:45 +0530

    changes 8nov

--- backend/index.js
+++ backend/index.js
@@ -2,21 +2,25 @@ const connectToMongo =require('./db');
 const cors = require('cors');
 const express = require('express')
 const app = express()
+const path = require('path'); ;
 
 connectToMongo();
 const port = 5000;
 
-app.use(cors({
-  origin : ["https://deploy-mern-1whq.vercel.app"],
-  methods : ["POST" , "GET" , "DELETE" , "PUT"],
-  credentials :true
-}));
+const  _dirname = path.resolve();
+app.use(cors());
 app.use(express.json())
 
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/transactions',require('./routes/transactions'));
 app.use('/api/category',require('./routes/category'));
 
+
+app.use(express.static(path.join(_dirname,"/frontend/build/")));
+
+app.get('*' , ((_,res) => {
+  res.sendFile(path.resolve(_dirname,"frontend","build","index.html"));
+}))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
